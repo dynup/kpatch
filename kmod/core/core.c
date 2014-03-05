330a08dd0d1913de3e586cfd2b2ea7616c3cd31b
Josh Poimboeuf <jpoimboe@redhat.com>
2014-03-04 21:34:03 -0600

    add GPLv2 headers to source files

--- kmod/core/core.c
+++ kmod/core/core.c
@@ -2,7 +2,23 @@
  * Copyright (C) 2014 Seth Jennings <sjenning@redhat.com>
  * Copyright (C) 2013 Josh Poimboeuf <jpoimboe@redhat.com>
  *
- * Contains the code for the core kpatch module.  This module reads
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA,
+ * 02110-1301, USA.
+ */
+
+/* Contains the code for the core kpatch module.  This module reads
  * information from the patch modules, find new patched functions,
  * and register those functions in the ftrace handlers the redirects
  * the old function call to the new function code.
--- kpatch-build/create-diff-object.c
+++ kpatch-build/create-diff-object.c
@@ -1,9 +1,26 @@
 /*
- * tools/create-diff-object.c
+ * create-diff-object.c
  *
  * Copyright (C) 2014 Seth Jennings <sjenning@redhat.com>
  * Copyright (C) 2013 Josh Poimboeuf <jpoimboe@redhat.com>
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA,
+ * 02110-1301, USA.
+ */
+
+/*
  * This file contains the heart of the ELF object differencing engine.
  *
  * The tool takes two ELF objects from two versions of the same source
