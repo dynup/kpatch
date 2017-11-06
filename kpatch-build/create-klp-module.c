dynup/kpatch

commit 5888f316e63a2cc460180e8ace12d45820cd0d19

create-klp-module: support unbundled symbols
The create_klp_relasecs_and_syms() function assumes that all dest
symbols are bundled, i.e. each symbol is located at offset 0 in its own
section.

However that may not always be the case.  Unbundled symbols can occur,
for example, when combining two .o files which have the same bundled
symbol.  They will be combined into the same section and will no longer
be considered "bundled".

Signed-off-by: Josh Poimboeuf <jpoimboe@redhat.com>

kpatch-build/create-klp-module.c
@@ -155,9 +155,9 @@ static struct section *find_or_add_klp_relasec(struct kpatch_elf *kelf,
 static void create_klp_relasecs_and_syms(struct kpatch_elf *kelf, struct section *krelasec,
 					 struct section *ksymsec, char *strings)
 {
-	struct section *base, *klp_relasec;
+	struct section *klp_relasec;
 	struct kpatch_relocation *krelas;
-	struct symbol *sym;
+	struct symbol *sym, *dest;
 	struct rela *rela;
 	char *objname;
 	int nr, index, offset, toc_offset;
@@ -207,9 +207,7 @@ static void create_klp_relasecs_and_syms(struct kpatch_elf *kelf, struct section
 		 */
 		toc_offset = rela->addend;
 
-		base = rela->sym->sec;
-		if (!base)
-			ERROR("base sec of krela not found");
+		dest = rela->sym;
 
 		/* Get the name of the object the rela belongs to */
 		rela = find_rela_by_offset(krelasec->rela,
@@ -232,8 +230,8 @@ static void create_klp_relasecs_and_syms(struct kpatch_elf *kelf, struct section
 		if (!sym)
 			ERROR("error finding or adding ksym to symtab");
 
-		/* Create (or find) the .klp.rela. section for this base sec and object */
-		klp_relasec = find_or_add_klp_relasec(kelf, base, objname);
+		/* Create (or find) the .klp.rela. section for this dest sec and object */
+		klp_relasec = find_or_add_klp_relasec(kelf, dest->sec, objname);
 		if (!klp_relasec)
 			ERROR("error finding or adding klp relasec");
 
@@ -241,10 +239,10 @@ static void create_klp_relasecs_and_syms(struct kpatch_elf *kelf, struct section
 		ALLOC_LINK(rela, &klp_relasec->relas);
 		rela->sym = sym;
 		rela->type = krelas[index].type;
-		if (!strcmp(base->name, ".toc"))
+		if (!strcmp(dest->sec->name, ".toc"))
 			rela->offset = toc_offset;
 		else
-			rela->offset = krelas[index].offset;
+			rela->offset = krelas[index].offset + dest->sym.st_value;
 
 		/*
 		 * GCC 6+ adds 0x8 to the offset of every local function entry
@@ -253,7 +251,7 @@ static void create_klp_relasecs_and_syms(struct kpatch_elf *kelf, struct section
 		 * local function becomes global, we don't want to skip the
 		 * .toc setup anymore.
 		 */
-		if (!strcmp(base->name, ".toc") &&
+		if (!strcmp(dest->sec->name, ".toc") &&
 			rela->sym->type == STT_FUNC && rela->sym->bind == STB_LOCAL) {
 				rela->addend = 0;
 		} else {

commit fb9a791e6d67bb86eb906426e4f77f2c4b50c653

ppc64le: Fix wrong offset for destination symbols
Commit 5888f316e63a ("create-klp-module: support unbundled symbols")
breaks the livepatch modules build on ppc64le, with stalls after
module load:

INFO: rcu_sched self-detected stall on CPU
   5-...: (21002 ticks this GP) idle=7ba/140000000000001/0
softirq=97524/97524 fqs=10499
    (t=21003 jiffies g=23955 c=23954 q=2203)
 NMI backtrace for cpu 5
 CPU: 5 PID: 22188 Comm: cat Tainted: G           OE K 4.14.0-rc7+ #3
 Call Trace:
 dump_stack+0xb0/0xf0 (unreliable)
 nmi_cpu_backtrace+0x208/0x210
 nmi_trigger_cpumask_backtrace+0x1c0/0x200
 arch_trigger_cpumask_backtrace+0x28/0x40
 rcu_dump_cpu_stacks+0xec/0x14c
 rcu_check_callbacks+0x908/0xb10
 update_process_times+0x48/0x90
 tick_sched_handle.isra.5+0x4c/0x80
 tick_sched_timer+0x60/0xe0
 __hrtimer_run_queues+0xf8/0x360
 hrtimer_interrupt+0xf8/0x330
 __timer_interrupt+0x94/0x270
 timer_interrupt+0xa4/0xe0
 decrementer_common+0x114/0x120
 --- interrupt: 901 at meminfo_proc_show+0x4c/0xd40
[livepatch_meminfo_string]
     LR = meminfo_proc_show+0x50/0xd40 [livepatch_meminfo_string]
 meminfo_proc_show+0x44/0xd40 [livepatch_meminfo_string] (unreliable)
 klp_stub_insn_end+0x4/0x38
 proc_reg_read+0x88/0xd0
 __vfs_read+0x44/0x1b0
  vfs_read+0xbc/0x1b0
 SyS_read+0x68/0x110
 system_call+0x58/0x6c

With GCC6+, every local function symbol has the value of 0x8 and
appending the symbol value to destination symbol, mis-calculates
the offset.

Fix the offset calculation, by subtracting symbol value 0x8 for
every destination functions, those are local functions as well.

Fixes: 5888f316e63a ("create-klp-module: support unbundled symbols")
Cc: Josh Poimboeuf <jpoimboe@redhat.com>
Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>

kpatch-build/create-klp-module.c
@@ -239,10 +239,19 @@ static void create_klp_relasecs_and_syms(struct kpatch_elf *kelf, struct section
 		ALLOC_LINK(rela, &klp_relasec->relas);
 		rela->sym = sym;
 		rela->type = krelas[index].type;
-		if (!strcmp(dest->sec->name, ".toc"))
+		if (!strcmp(dest->sec->name, ".toc")) {
 			rela->offset = toc_offset;
-		else
+		} else {
 			rela->offset = krelas[index].offset + dest->sym.st_value;
+			/*
+			 * With GCC 6+ every local function symbol has the value of 0x8.
+			 * If the destination sym is local function, don't add the
+			 * sym value to the offset.
+			 */
+			if (dest->sym.st_value &&
+			    dest->type == STT_FUNC && dest->bind == STB_LOCAL)
+				rela->offset -= 8;
+		}
 
 		/*
 		 * GCC 6+ adds 0x8 to the offset of every local function entry