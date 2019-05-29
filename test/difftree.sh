d91b416b00ee329e0ed91c8acf58fcadd5a316d8
chenzefeng <chenzefeng2@huawei.com>
2019-05-29 22:14:05 +0800

    test: use readelf instead of eu-readelf
    readelf is more standard, using readelf insteaded
    
    test/difftree.sh: the symbol name may be with "FILE", which may be
    get a incorrect count, here add "awk".
    
    test/unit/Makefile.include: use "readelf -s --wide" instead of
    "eu-readelf -s".
    
    Signed-off-by: chenzefeng <chenzefeng2@huawei.com>

--- test/difftree.sh
+++ test/difftree.sh
@@ -46,7 +46,7 @@ do
 		;;
 	esac
 	# skip objects that are the linked product of more than one object file
-	[[ $(eu-readelf -s $i | grep FILE | wc -l) -ne 1 ]] && continue
+	[[ $(readelf -s $i | awk '$4=="FILE" {n++} END {print n}') -ne 1 ]] && continue
 	$SCRIPTDIR/../kpatch-build/create-diff-object $i $i /usr/lib/debug/lib/modules/$(uname -r)/vmlinux "$TEMPDIR/output.o" > "$TEMPDIR/log.txt" 2>&1
 	RETCODE=$?
 	# expect RETCODE to be 3 indicating no change
