dynup/kpatch

commit 767d9669bd1387f3337db7a6a66bc999634e605a

kpatch-build: use readelf instead of eu-readelf
readelf is more standard, using readelf insteaded we should solve there
issues:

First, using "readelf -s", the symbol name would truncated by 25 chars,
to solve this issue, add option "--wide".

Second, the size may be mixed of decimal and hex, we get the size by "%s",
and use strtoul(size, NULL, 0) to convert the size.

Third, the symbol type is SHN_UNDE, the Ndx display "UND", so changed to
compare with "UND".

Signed-off-by: chenzefeng <chenzefeng2@huawei.com>

kpatch-build/lookup.c
@@ -200,17 +200,17 @@ static char *make_modname(char *modname)
 static void symtab_read(struct lookup_table *table, char *path)
 {
 	FILE *file;
-	long unsigned int value, size;
+	long unsigned int value;
 	unsigned int i = 0;
 	int matched;
-	char line[256], name[256], type[16], bind[16], ndx[16];
+	char line[256], name[256], size[16], type[16], bind[16], ndx[16];
 
 	if ((file = fopen(path, "r")) == NULL)
 		ERROR("fopen");
 
 	while (fgets(line, 256, file)) {
-		matched = sscanf(line, "%*s %lx %lu %s %s %*s %s %s\n",
-				 &value, &size, type, bind, ndx, name);
+		matched = sscanf(line, "%*s %lx %s %s %s %*s %s %s\n",
+				 &value, size, type, bind, ndx, name);
 
 		if (matched == 5) {
 			name[0] = '\0';
@@ -218,7 +218,7 @@ static void symtab_read(struct lookup_table *table, char *path)
 		}
 
 		if (matched != 6 ||
-		    !strcmp(ndx, "UNDEF") ||
+		    !strcmp(ndx, "UND") ||
 		    !strcmp(type, "SECTION"))
 			continue;
 
@@ -233,8 +233,8 @@ static void symtab_read(struct lookup_table *table, char *path)
 	rewind(file);
 
 	while (fgets(line, 256, file)) {
-		matched = sscanf(line, "%*s %lx %lu %s %s %*s %s %s\n",
-				 &value, &size, type, bind, ndx, name);
+		matched = sscanf(line, "%*s %lx %s %s %s %*s %s %s\n",
+				 &value, size, type, bind, ndx, name);
 
 		if (matched == 5) {
 			name[0] = '\0';
@@ -242,12 +242,12 @@ static void symtab_read(struct lookup_table *table, char *path)
 		}
 
 		if (matched != 6 ||
-		    !strcmp(ndx, "UNDEF") ||
+		    !strcmp(ndx, "UND") ||
 		    !strcmp(type, "SECTION"))
 			continue;
 
 		table->obj_syms[i].value = value;
-		table->obj_syms[i].size = size;
+		table->obj_syms[i].size = strtoul(size, NULL, 0);
 
 		if (!strcmp(bind, "LOCAL")) {
 			table->obj_syms[i].bind = STB_LOCAL;

kpatch-build/kpatch-build
@@ -845,7 +845,7 @@ for i in $FILES; do
 			SYMVERS_FILE="$SRCDIR/Module.symvers"
 		fi
 
-		eu-readelf -s "$KOBJFILE_PATH" > "$SYMTAB"
+		readelf -s --wide "$KOBJFILE_PATH" > "$SYMTAB"
 
 		# create-diff-object orig.o patched.o parent-name parent-symtab
 		#		     Module.symvers patch-mod-name output.o

commit f8213c87f64a10f0315dfad8f66091137ed07cd0

lookup: Fix format string for symtab_read() on PPC64LE
commit 767d9669bd13 ("kpatch-build: use readelf instead of eu-readelf")
replaced eu-readelf with readelf for constructing symbol table.  The
format of symbol table entries differs a little on Power when the symbol
is a function with binding type LOCAL.  For example, consider:

23: 0000000000000008    96 FUNC    LOCAL  DEFAULT [<localentry>: 8]    4 cmdline_proc_show

An extra column preceding index of the symbol denoting symbol value to
be local entry point offset of the function is printed, with the
current sscanf format string in lookup::symtab_read the values will
mismatch ending with in accurate lookup table getting constructed. This
patch fixes it by introducing an Power specific format string for
function symbols with bind type LOCAL.

Fixes: 767d9669 ("kpatch-build: use readelf instead of eu-readelf")
Signed-off-by: Kamalesh Babulal <kamalesh@linux.vnet.ibm.com>

kpatch-build/lookup.c
@@ -209,8 +209,18 @@ static void symtab_read(struct lookup_table *table, char *path)
 		ERROR("fopen");
 
 	while (fgets(line, 256, file)) {
-		matched = sscanf(line, "%*s %lx %s %s %s %*s %s %s\n",
-				 &value, size, type, bind, ndx, name);
+		if (strstr(line, "[<localentry>: 8]")) {
+			/*
+			 * 23: 0000000000000008    96 FUNC    LOCAL  DEFAULT [<localentry>: 8]     4 cmdline_proc_show
+			 * On Power, local func syms Ndx is preceded with "[<localentry>: 8]"
+			 * denoting local entry point offset of the function.
+			 */
+			matched = sscanf(line, "%*s %lx %s %s %s %*s [<localentry>: 8] %s %s\n",
+					 &value, size, type, bind, ndx, name);
+		} else {
+			matched = sscanf(line, "%*s %lx %s %s %s %*s %s %s\n",
+					 &value, size, type, bind, ndx, name);
+		}
 
 		if (matched == 5) {
 			name[0] = '\0';
@@ -233,8 +243,13 @@ static void symtab_read(struct lookup_table *table, char *path)
 	rewind(file);
 
 	while (fgets(line, 256, file)) {
-		matched = sscanf(line, "%*s %lx %s %s %s %*s %s %s\n",
-				 &value, size, type, bind, ndx, name);
+		if (strstr(line, "[<localentry>: 8]")) {
+			matched = sscanf(line, "%*s %lx %s %s %s %*s [<localentry>: 8] %s %s\n",
+					 &value, size, type, bind, ndx, name);
+		} else {
+			matched = sscanf(line, "%*s %lx %s %s %s %*s %s %s\n",
+					 &value, size, type, bind, ndx, name);
+		}
 
 		if (matched == 5) {
 			name[0] = '\0';