dynup/kpatch

commit b913b4bae0897475ad89eddb8d4e32f1d4ac56f6

livepatch-patch-hook: skip klp_(un)register_patch() for RHEL-7.8+ kernels
RHEL-7.8 backported upstream kernel commit 958ef1e39d24 ("livepatch:
Simplify API by removing registration step"), so add it to the kernels
using the simple registration API.

Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

kmod/patch/livepatch-patch-hook.c
@@ -64,7 +64,11 @@
 # define HAVE_CALLBACKS
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0)
+#ifdef RHEL_RELEASE_CODE
+# if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7, 8)
+#  define HAVE_SIMPLE_ENABLE
+# endif
+#elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0)
 # define HAVE_SIMPLE_ENABLE
 #endif
 

commit 36120b9b08ec63242920622e796658e816af35f7

kmod/patch: check for RHEL8 when setting HAVE_SIMPLE_ENABLE
Make sure we don't set HAVE_SIMPLE_ENABLE on rhel8 before rhel8.2

Fixes: b913b4b ("livepatch-patch-hook: skip klp_(un)register_patch() for RHEL-7.8+ kernels")
Fixes: #1031

Signed-off-by: Artem Savkov <asavkov@redhat.com>

kmod/patch/livepatch-patch-hook.c
@@ -65,7 +65,9 @@
 #endif
 
 #ifdef RHEL_RELEASE_CODE
-# if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7, 8)
+# if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(7, 8) && 		\
+	 RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(8, 0)) || 		\
+      RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(8, 2)
 #  define HAVE_SIMPLE_ENABLE
 # endif
 #elif LINUX_VERSION_CODE >= KERNEL_VERSION(5, 1, 0)