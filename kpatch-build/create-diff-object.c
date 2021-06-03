ba3defa06073dcc69917dbbbb9df570ca4e56612
Bill Wendling <morbo@google.com>
2021-06-03 01:41:35 -0700

    create-diff-object: Check that the section has a secsym
    A STT_SECTION symbol is not needed if if it is not used as a relocation
    target. Therefore, a section, in this case a debug section, may not have
    a secsym associated with it.
    
    Signed-off-by: Bill Wendling <morbo@google.com>

--- kpatch-build/create-diff-object.c
+++ kpatch-build/create-diff-object.c
@@ -2564,7 +2564,7 @@ static void kpatch_include_debug_sections(struct kpatch_elf *kelf)
 	list_for_each_entry(sec, &kelf->sections, list) {
 		if (is_debug_section(sec)) {
 			sec->include = 1;
-			if (!is_rela_section(sec))
+			if (!is_rela_section(sec) && sec->secsym)
 				sec->secsym->include = 1;
 		}
 	}
