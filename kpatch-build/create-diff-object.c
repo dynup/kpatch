5074275b5db0b13c652ef3fd543a3b43bfe8026f
Hongchen Zhang <zhanghongchen@loongson.cn>
2022-11-17 09:56:23 +0800

    optimize section symbol replace algorithm
    Let's do R_X86_64_32S judgment firstly, so other architectures
    would run a little faster.
    
    Signed-off-by: Hongchen Zhang <zhanghongchen@loongson.cn>

--- kpatch-build/create-diff-object.c
+++ kpatch-build/create-diff-object.c
@@ -1558,9 +1558,9 @@ static void kpatch_replace_sections_syms(struct kpatch_elf *kelf)
 				start = sym->sym.st_value;
 				end = sym->sym.st_value + sym->sym.st_size;
 
-				if (is_text_section(relasec->base) &&
+				if (rela->type == R_X86_64_32S &&
+				    is_text_section(relasec->base) &&
 				    !is_text_section(sym->sec) &&
-				    rela->type == R_X86_64_32S &&
 				    rela->addend == (long)sym->sec->sh.sh_size &&
 				    end == (long)sym->sec->sh.sh_size) {
 
