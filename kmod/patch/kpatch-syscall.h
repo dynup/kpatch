dynup/kpatch

commit e42c7990ca1db370fef5b4187b4078fc79ae8d6f

kpatch-syscall: update for s390x RHEL-9.6 backports
RHEL-9.6 backported the upstream v6.3 s390x syscall updates, so add a
distro-specific kernel version check around the correct definitions.

Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

kmod/patch/kpatch-syscall.h
@@ -91,40 +91,23 @@
 
 #elif defined(CONFIG_S390)
 
-/* arch/s390/include/asm/syscall_wrapper.h versions */
 # if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
-
-#define __KPATCH_S390_SYS_STUBx(x, name, ...)                                          \
-	long __s390_sys##name(struct pt_regs *regs);                            \
-	ALLOW_ERROR_INJECTION(__s390_sys##name, ERRNO);                         \
-	static inline long ___se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__));   \
-	long __s390_sys##name(struct pt_regs *regs)                             \
-	{                                                                       \
-		return ___se_sys##name(SC_S390_REGS_TO_ARGS(x, __VA_ARGS__));   \
-	}                                                                       \
-	static inline long ___se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__))    \
-	{                                                                       \
-		__MAP(x, __SC_TEST, __VA_ARGS__);                               \
-		return __kpatch_do_sys##name(__MAP(x, __SC_COMPAT_CAST, __VA_ARGS__)); \
-	}
-
-#define __KPATCH_SYSCALL_DEFINEx(x, name, ...)					\
-       long __s390x_sys##name(struct pt_regs *regs);                           \
-       ALLOW_ERROR_INJECTION(__s390x_sys##name, ERRNO);                        \
-       static inline long __se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__));    \
-       static inline long __kpatch_do_sys##name(__MAP(x, __SC_DECL, __VA_ARGS__));    \
-       __KPATCH_S390_SYS_STUBx(x, name, __VA_ARGS__);                                 \
-       long __s390x_sys##name(struct pt_regs *regs)                            \
-       {                                                                       \
-               return __se_sys##name(SC_S390_REGS_TO_ARGS(x, __VA_ARGS__));    \
-       }                                                                       \
-       static inline long __se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__))     \
-       {                                                                       \
-               __MAP(x, __SC_TEST, __VA_ARGS__);                               \
-               return __kpatch_do_sys##name(__MAP(x, __SC_CAST, __VA_ARGS__));        \
-       }                                                                       \
-       static inline long __kpatch_do_sys##name(__MAP(x, __SC_DECL, __VA_ARGS__))
+#  define KPATCH_SYSCALL_WRAPPERS_V2
 # else
+#  define KPATCH_SYSCALL_WRAPPERS_V1
+# endif
+
+# if defined(RHEL_RELEASE_CODE)
+#  if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 6)
+#   define KPATCH_SYSCALL_WRAPPERS_V2
+#  else
+#   define KPATCH_SYSCALL_WRAPPERS_V1
+#  endif
+# endif
+
+
+/* arch/s390/include/asm/syscall_wrapper.h versions */
+#if defined(KPATCH_SYSCALL_WRAPPERS_V1)
 
 #define __KPATCH_S390_SYS_STUBx(x, name, ...)					\
 	long __s390_sys##name(struct pt_regs *regs);				\
@@ -157,7 +140,40 @@
 	__diag_pop();									\
 	static inline long __kpatch_do_sys##name(__MAP(x,__SC_DECL,__VA_ARGS__))
 
-#endif /* LINUX_VERSION_CODE */
+# else /* KPATCH_SYSCALL_WRAPPERS_V2 */
+
+#define __KPATCH_S390_SYS_STUBx(x, name, ...)                                          \
+	long __s390_sys##name(struct pt_regs *regs);                            \
+	ALLOW_ERROR_INJECTION(__s390_sys##name, ERRNO);                         \
+	static inline long ___se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__));   \
+	long __s390_sys##name(struct pt_regs *regs)                             \
+	{                                                                       \
+		return ___se_sys##name(SC_S390_REGS_TO_ARGS(x, __VA_ARGS__));   \
+	}                                                                       \
+	static inline long ___se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__))    \
+	{                                                                       \
+		__MAP(x, __SC_TEST, __VA_ARGS__);                               \
+		return __kpatch_do_sys##name(__MAP(x, __SC_COMPAT_CAST, __VA_ARGS__)); \
+	}
+
+#define __KPATCH_SYSCALL_DEFINEx(x, name, ...)					\
+       long __s390x_sys##name(struct pt_regs *regs);                           \
+       ALLOW_ERROR_INJECTION(__s390x_sys##name, ERRNO);                        \
+       static inline long __se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__));    \
+       static inline long __kpatch_do_sys##name(__MAP(x, __SC_DECL, __VA_ARGS__));    \
+       __KPATCH_S390_SYS_STUBx(x, name, __VA_ARGS__);                                 \
+       long __s390x_sys##name(struct pt_regs *regs)                            \
+       {                                                                       \
+               return __se_sys##name(SC_S390_REGS_TO_ARGS(x, __VA_ARGS__));    \
+       }                                                                       \
+       static inline long __se_sys##name(__MAP(x, __SC_LONG, __VA_ARGS__))     \
+       {                                                                       \
+               __MAP(x, __SC_TEST, __VA_ARGS__);                               \
+               return __kpatch_do_sys##name(__MAP(x, __SC_CAST, __VA_ARGS__));        \
+       }                                                                       \
+       static inline long __kpatch_do_sys##name(__MAP(x, __SC_DECL, __VA_ARGS__))
+
+#endif /* KPATCH_SYSCALL_WRAPPERS_V2 */
 
 #elif defined(CONFIG_PPC64)
 

commit 34d23c72254cd24c9d74cb4727f0108c04bc64b1

kpatch-syscall: fix rhel-9.6 KPATCH_SYSCALL_WRAPPERS
In RHEL-9.6, both KPATCH_SYSCALL_WRAPPERS_V1 and
KPATCH_SYSCALL_WRAPPERS_V2 are defined due to the order of the
preprocessor defines:

 (Consider a RHEL-9.6 kernel version of 5.14.0-570.12.1)

 # if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
 #  define KPATCH_SYSCALL_WRAPPERS_V2
 # else
 #  define KPATCH_SYSCALL_WRAPPERS_V1                      << here
 # endif

 # if defined(RHEL_RELEASE_CODE)
 #  if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 6)
 #   define KPATCH_SYSCALL_WRAPPERS_V2                     << and here
 #  else
 #   define KPATCH_SYSCALL_WRAPPERS_V1
 #  endif

Re-arrange the conditionals such that only downstream and upstream are
considered exclusive of one another.

Fixes: e42c7990ca1d ("kpatch-syscall: update for s390x RHEL-9.6 backports")
Signed-off-by: Joe Lawrence <joe.lawrence@redhat.com>

kmod/patch/kpatch-syscall.h
@@ -91,20 +91,19 @@
 
 #elif defined(CONFIG_S390)
 
-# if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
-#  define KPATCH_SYSCALL_WRAPPERS_V2
-# else
-#  define KPATCH_SYSCALL_WRAPPERS_V1
-# endif
-
 # if defined(RHEL_RELEASE_CODE)
 #  if RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(9, 6)
 #   define KPATCH_SYSCALL_WRAPPERS_V2
 #  else
 #   define KPATCH_SYSCALL_WRAPPERS_V1
 #  endif
+# else
+#  if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0)
+#   define KPATCH_SYSCALL_WRAPPERS_V2
+#  else
+#   define KPATCH_SYSCALL_WRAPPERS_V1
+#  endif
 # endif
-
 
 /* arch/s390/include/asm/syscall_wrapper.h versions */
 #if defined(KPATCH_SYSCALL_WRAPPERS_V1)