d876469f25036628b685a98d3fbea94bca3fda64
lu21253 <lu.haoA@h3c.com>
2026-05-15 14:44:08 +0800

    create-diff-object: delete section if sym->incluce == 0
    Create a patch file. When confirming which debug sections to include,
    there is a scenario where rela->sym->sec->include is 1,
    but rela->sym->include is 0. This indicates that the section containing
    the symbol needs to be included in the patch, but the
    symbol itself does not need to be included. During linking, the
    following error will occur:
    bad reloc symbol index (0x47c01 >= 0x9f) for offset 0x822f in section
    `.debug_info'
    
    Signed-off-by: lu21253 <lu.haoA@h3c.com>

--- kpatch-build/create-diff-object.c
+++ kpatch-build/create-diff-object.c
@@ -3054,7 +3054,7 @@ static void kpatch_include_debug_sections(struct kpatch_elf *kelf)
 		if (!is_rela_section(sec) || !is_debug_section(sec))
 			continue;
 		list_for_each_entry_safe(rela, saferela, &sec->relas, list)
-			if (!rela->sym->sec->include)
+			if (!rela->sym->sec->include || !rela->sym->include)
 				list_del(&rela->list);
 	}
 }
